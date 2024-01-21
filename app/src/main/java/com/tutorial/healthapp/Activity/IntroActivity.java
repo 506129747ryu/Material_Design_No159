2b9a9b130b9860e817ef7cee0ba5e385ab0471d8
506129747ryu <506129747ryu@gmail.com>
2024-01-21 16:03:38 +0900

    add Intro Activity

---
+++ app/src/main/java/com/tutorial/healthapp/Activity/IntroActivity.java
@@ -0,0 +1,15 @@
+package com.tutorial.healthapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tutorial.healthapp.R;
+
+public class IntroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
