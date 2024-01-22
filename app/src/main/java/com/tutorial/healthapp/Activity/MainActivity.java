2a139934df43c9442a6629336d9fc57b04d808b0
506129747ryu <506129747ryu@gmail.com>
2024-01-22 15:42:05 +0900

    add Best Deal Backend

--- app/src/main/java/com/tutorial/healthapp/Activity/MainActivity.java
+++ app/src/main/java/com/tutorial/healthapp/Activity/MainActivity.java
@@ -8,8 +8,10 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.tutorial.healthapp.Adapter.BestDealAdapter;
 import com.tutorial.healthapp.Adapter.CategoryAdapter;
 import com.tutorial.healthapp.Domain.CategoryDomain;
+import com.tutorial.healthapp.Domain.ItemsDomain;
 import com.tutorial.healthapp.R;
 
 import java.util.ArrayList;
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         initRecylerViewCat();
         initLocation();
+        initRecyclerViewBestDeal();
 
     }
 
@@ -57,6 +60,30 @@ public class MainActivity extends AppCompatActivity {
 
         catAdapter = new CategoryAdapter(items);
         recyclerViewCat.setAdapter(catAdapter);
+    }
+
+    private void initRecyclerViewBestDeal(){
+        recyclerViewBestDeal = findViewById(R.id.bestView);
+        recyclerViewBestDeal.setLayoutManager(new LinearLayoutManager(
+                this,
+                LinearLayoutManager.HORIZONTAL,
+                false
+        ));
+
+        bestDealAdapter = new BestDealAdapter(getData());
+        recyclerViewBestDeal.setAdapter(bestDealAdapter);
+    }
+
+    public ArrayList<ItemsDomain> getData(){
+        ArrayList<ItemsDomain> items = new ArrayList<>();
+        items.add(new ItemsDomain("orange","Fresh Orange",6.2,"With its vibrant orange hue and a burst of refreshing citrus flavor, the juicy orange is a natural source of vitamin C, invigorating your senses and supporting your immune system. A delightful snack that combines health and taste.",4));
+        items.add(new ItemsDomain("apple","Fresh Apple",6.5,"With its vibrant orange hue and a burst of refreshing citrus flavor, the juicy orange is a natural source of vitamin C, invigorating your senses and supporting your immune system. A delightful snack that combines health and taste.",4.8));
+        items.add(new ItemsDomain("watermelon","Fresh Watermelon",7.0,"With its vibrant orange hue and a burst of refreshing citrus flavor, the juicy orange is a natural source of vitamin C, invigorating your senses and supporting your immune system. A delightful snack that combines health and taste.",5.0));
+        items.add(new ItemsDomain("berry","Fresh Berry",10.3,"With its vibrant orange hue and a burst of refreshing citrus flavor, the juicy orange is a natural source of vitamin C, invigorating your senses and supporting your immune system. A delightful snack that combines health and taste.",3.0));
+        items.add(new ItemsDomain("pineapple","Fresh Pineapple",3.5,"With its vibrant orange hue and a burst of refreshing citrus flavor, the juicy orange is a natural source of vitamin C, invigorating your senses and supporting your immune system. A delightful snack that combines health and taste.",4.1));
+        items.add(new ItemsDomain("strawberry","Fresh Strawberry",15.0,"With its vibrant orange hue and a burst of refreshing citrus flavor, the juicy orange is a natural source of vitamin C, invigorating your senses and supporting your immune system. A delightful snack that combines health and taste.",2.0));
+
+        return items;
     }
 
 }
